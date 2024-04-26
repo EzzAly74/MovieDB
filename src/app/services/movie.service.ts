ae6e446ece4ea70b925b5333d82f468fcfbd6a6c
omarkhaled <okoros192000@gmail.com>
2024-04-26 19:07:51 +0300

    make a test for trending section

--- src/app/services/movie.service.ts
+++ src/app/services/movie.service.ts
@@ -8,7 +8,8 @@ export class MovieService {
   constructor(private http: HttpClient) {}
   baseUrl: string = 'https://api.themoviedb.org/3';
   params: any = {
-    api_key: '017e587b73b558aeca164c84108948ad',
+    // api_key: '017e587b73b558aeca164c84108948ad',
+    api_key: 'c86076a9568df4a4395e1879c7d55cae',
   };
 
   getNowPlaying(): Observable<any> {
